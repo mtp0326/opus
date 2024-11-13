252abe83ddcb8649d5a99cb9fbc30e3e90c02509
opham128 <Opham@seas.upenn.edu>
2024-11-12 21:52:37 -0500

    survey builder

--- client/src/Projects/SurveyBuilder.tsx
+++ client/src/Projects/SurveyBuilder.tsx
@@ -1,86 +1,18 @@
-import React, { useState } from 'react';
-import styles from './SurveyBuilder.module.css';
+// Uncomment the following line if you are using Next.js:
+// 'use client'
 
-interface Question {
-  id: string;
-  type: 'multiple_choice' | 'text' | 'rating' | 'checkbox';
-  question: string;
-  options?: string[];
-  required: boolean;
-}
+import { SurveyCreatorComponent, SurveyCreator } from 'survey-creator-react';
+import 'survey-core/defaultV2.min.css';
+import 'survey-creator-core/survey-creator-core.min.css';
 
-interface SurveyData {
-  title: string;
-  description: string;
-  reward: string;
-  respondents: string;
-  timeToComplete: string;
-  expiresIn: string;
-  workerQualifications: string;
-  questions: Question[];
-}
-
-const SurveyBuilder = () => {
-  const [surveyData, setSurveyData] = useState<SurveyData>({
-    title: '',
-    description: '',
-    reward: '',
-    respondents: '',
-    timeToComplete: '',
-    expiresIn: '',
-    workerQualifications: 'basic',
-    questions: []
-  });
-
-  const [currentQuestion, setCurrentQuestion] = useState<Question>({
-    id: '',
-    type: 'multiple_choice',
-    question: '',
-    options: [''],
-    required: false
-  });
-
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleBasicInfoChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setSurveyData(prev => ({
-      ...prev,
-      [name]: value
-    }));
-  };
-  const handleQuestionChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    const checked = (e.target as HTMLInputElement).checked;
-    const type = e.target.type;
-    setCurrentQuestion(prev => ({
-      ...prev,
-      [name]: type === 'checkbox' ? checked : value
-    }));
-  };
-
-  const handleOptionChange = (index: number, value: string) => {
-    setCurrentQuestion(prev => ({
-      ...prev,
-      options: prev.options?.map((opt, i) => (i === index ? value : opt)) || []
-    }));
-  };
-
-  const addOption = () => {
-    setCurrentQuestion(prev => ({
-      ...prev,
-      options: [...(prev.options || []), '']
-    }));
-  };
-
-  return (
-    <div className={styles.surveyBuilder}>
-      {/* Render your survey components here */}
-    </div>
-  );
+const creatorOptions = {
+  showLogicTab: true,
+  isAutoSave: true
 };
 
-export default SurveyBuilder; 
+export function SurveyBuilder() {
+  const creator = new SurveyCreator(creatorOptions);
+  return (
+    <SurveyCreatorComponent creator={creator} />
+  )
+}
